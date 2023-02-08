5eea8eebdd06f649d92170c198ccb82390bd18d0
Griffin Gradke <griffin.gradke@gmail.com>
2023-02-08 14:55:18 -0600

    fetching data on page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -35,7 +35,25 @@ function App() {
     }
     
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if(movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if(error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading your results...</p>;
+  }
 
   return (
     <React.Fragment>
@@ -43,10 +61,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found No Results</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading your results...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
